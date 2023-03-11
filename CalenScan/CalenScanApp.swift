8ba33105db3b06ca42a775950ba262621ce5ac2a
Michał Reclik <michal-reclik@wp.pl>
2023-03-11 18:01:31 +0100

    init

---
+++ CalenScan/CalenScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalenScanApp.swift
+//  CalenScan
+//
+//  Created by Reclocco on 11/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalenScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
